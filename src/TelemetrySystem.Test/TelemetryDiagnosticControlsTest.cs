4e9b66e8652dfbf71425bf2a5901a73bcecc012c
codedineshgupta <code.dineshgupta@gmail.com>
2023-07-24 09:37:03 +0530

    setup with legacy code and test project

---
+++ src/TelemetrySystem.Test/TelemetryDiagnosticControlsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetrySystem.Test
+{
+    public class TelemetryDiagnosticControlsTest
+    {
+        [Fact]
+        public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+        {
+        }
+    }
+}
